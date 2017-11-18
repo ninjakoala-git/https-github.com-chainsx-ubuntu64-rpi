0acfae76425838ed6785b132567459ddbd7740c5
chainsx <chainsx@users.noreply.github.com>
2017-11-18 00:04:18 -0600

    Update install.py

--- prepared/install.py
+++ prepared/install.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python
 #Filename:install.py
+import os
+print('untar files')
+os.popen('tar -zxvf Image.tar.gz')
+os.popen('tar -zxvf dtbs.tar.gz')
+os.popen('tar -zxvf modiles.tar.gz')
+os.popen('tar -zxvf firmware.tar.gz')
