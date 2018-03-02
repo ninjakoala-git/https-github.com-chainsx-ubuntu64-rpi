cdf6263deaa808e0ce493d9f615e7099194b22db
chainsx(cx-dandelion) <chainsx@users.noreply.github.com>
2018-03-02 02:08:23 -0600

    Update build-kernel.sh

--- build/build-kernel.sh
+++ build/build-kernel.sh
@@ -1,5 +1,6 @@
 wget https://github.com/raspberrypi/linux/archive/rpi-4.11.y.zip
 unzip rpi*
 cd linux*
-make bcmrpi3_defconfig
-make -j4 ARCH=arm64
+sudo apt-get install gcc-aarch64-libux-gnu
+make bcmrpi3_defconfig CROSS_COMPILE=aarch64-linux-gnu-
+make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
